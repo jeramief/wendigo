f18f72f3ce86e9c90c361455911c2ccd7a4b2b9d
Jeramie <forbesjeramie@gmail.com>
2024-06-06 11:51:39 -0400

    cars list looking better

--- frontend/src/components/Cars/CarCard.jsx
+++ frontend/src/components/Cars/CarCard.jsx
@@ -11,14 +11,13 @@ const CarCard = ({ car }) => {
 
   return (
     <div className="car-card" onClick={() => onClick(car.id)}>
-      <ShowImage
-        url={car.image}
-        type={"vehicle-card"}
-        style={{ width: "100px" }}
-      />
+      <ShowImage url={car.image} type={"vehicle-card"} />
       <div className="car-card-content">
-        <h4>{`${car.year} ${car.make} ${car.model}`}</h4>
-        <h3>{car.price}</h3>
+        <div>
+          <p className="car-card-text">{`${car.year} ${car.make} ${car.model}`}</p>
+          <p className="car-card-text">Mileage: {car.mileage}</p>
+        </div>
+        <h3 className="car-card-text car-card-price">Price: ${car.price}</h3>
       </div>
     </div>
   );
