7341e3b274093888f9b99afa8514affe4d0105c2
Jeramie <forbesjeramie@gmail.com>
2024-06-10 02:19:45 -0400

    reviews carousel working

--- frontend/src/components/Reviews/ReviewsCarousel.jsx
+++ frontend/src/components/Reviews/ReviewsCarousel.jsx
@@ -1,11 +1,49 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
+import { thunkLoadReviews } from "../../redux/reviewsReducer";
 import OpenModalButton from "../OpenModalButton";
 import ReviewsModal from "./ReviewsModal";
 
 const ReviewsCarousel = ({ car }) => {
-  const reviews = useSelector((state) => Object.values(state));
+  const dispatch = useDispatch();
+
+  const reviews = useSelector((state) =>
+    Object.values(state.reviewsState).filter(
+      (review) => review.vehicleId === car?.id
+    )
+  );
+
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [reviewPosition, setReviewPosition] = useState(0);
+
+  useEffect(() => {
+    dispatch(thunkLoadReviews())
+      .then(() => setIsLoaded(true))
+      .catch((errors) => {
+        console.log({ errors });
+      });
+  }, [dispatch]);
+
+  const previousReview = () => {
+    if (reviews.length === 0) return;
+    else if (reviewPosition === 0) {
+      setReviewPosition(reviews.length - 1);
+    } else if (reviewPosition > 0) {
+      setReviewPosition(reviewPosition - 1);
+    }
+  };
+
+  const nextReview = () => {
+    if (reviews.length === 0) {
+      return;
+    } else if (reviewPosition === reviews.length - 1) {
+      setReviewPosition(0);
+    } else if (reviewPosition < reviews.length - 1) {
+      setReviewPosition(reviewPosition + 1);
+    }
+  };
 
   return (
     <div className="review-carousel">
@@ -15,11 +53,21 @@ const ReviewsCarousel = ({ car }) => {
         modalComponent={<ReviewsModal car={car} />}
       />
       <div className="review-carousel-reveiws-container">
-        <button className="review-carousel-left-arrow">
+        <button className="review-carousel-left-arrow" onClick={previousReview}>
           <IoIosArrowBack />
         </button>
-        <div className="review-carousel-reviews"></div>
-        <button className="review-carousel-right-arrow">
+        {isLoaded && reviews.length ? (
+          <div className="review-carousel-reviews">
+            <p>{reviews[reviewPosition].userName}</p>
+            <p>{reviews[reviewPosition].vehicleType}</p>
+            <p>{reviews[reviewPosition].commentText}</p>
+          </div>
+        ) : (
+          <div className="review-carousel-reviews">
+            <h3>No Reviews</h3>
+          </div>
+        )}
+        <button className="review-carousel-right-arrow" onClick={nextReview}>
           <IoIosArrowForward />
         </button>
       </div>
