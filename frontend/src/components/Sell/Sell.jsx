76207aa301521ada84dd239e288628fd44ec8c90
Jeramie <forbesjeramie@gmail.com>
2024-06-14 21:16:42 -0400

    working on sell form

--- frontend/src/components/Sell/Sell.jsx
+++ frontend/src/components/Sell/Sell.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
 import "./Sell.css";
+import { useModal } from "../../context/Modal";
 
 const Sell = () => {
-  return <div className="sell-form-container">Sell</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
+  currentUser = useSelector((state) => state.session.user);
+
+  const [VIN, setVIN] = useState('')
+  
+
+  return (
+    <form className="sell-form-container">
+      <label>
+        VIN
+        <input type="text" />
+      </label>
+    </form>
+  );
 };
 
 export default Sell;
